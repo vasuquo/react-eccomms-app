fd80e7d87c0683a4d31a32ccfff2f749205c60db
veasuquo@gmail.com <41117686+vasuquo@users.noreply.github.com>
2021-10-07 10:29:11 +0100

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id: 1,
+            name: 'Nice slim shirt',
+            category: 'shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReview: 10,
+            description: 'High quality shirt'
+        },
+        {
+            _id: 2,
+            name: 'Trendy suit shirt and tie',
+            category: 'suit',
+            image: '/images/p2.jpg',
+            price: 250,
+            brand: 'Gucci',
+            rating: 3.5,
+            numReview: 15,
+            description: 'High quality shirt'
+        },
+        {
+            _id: 3,
+            name: 'Adidas slim shirt',
+            category: 'shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 2.5,
+            numReview: 10,
+            description: 'High quality shirt'
+        },
+        {
+            _id: 4,
+            name: 'Stgami slim shirt',
+            category: 'shirts',
+            image: '/images/p4.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 3.5,
+            numReview: 10,
+            description: 'High quality shirt'
+        },
+        {
+            _id: 5,
+            name: 'Amani slim shirt',
+            category: 'shirts',
+            image: '/images/p5.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 3.0,
+            numReview: 10,
+            description: 'High quality shirt'
+        },
+        {
+            _id: 6,
+            name: 'Clime slim shirt',
+            category: 'shirts',
+            image: '/images/p6.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.0,
+            numReview: 10,
+            description: 'High quality shirt'
+        },
+
+    ],
+};
+export default data;
